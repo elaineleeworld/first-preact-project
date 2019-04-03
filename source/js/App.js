1255840df41e87a29f38e84e783bae807e8ea48c
elaineleeworld <elaine.lee@elainel-mbp.ff0000.com>
2019-04-03 15:26:48 -0700

    add file loader to load woff fonts

--- source/js/App.js
+++ source/js/App.js
@@ -12,7 +12,10 @@ class App extends Component {
 				 <img className='red-logo' src='assets/images/REDproduction.png'/>
 				 <div className='copy-text'>
 				 <h1>Ads with impact.</h1>
+				 <br/>
 				 <p>For most agencies, display and social ads are an afterthought. For us, they're our story.  We know how to make people stop scrolling - and start interacting.</p>
+				 <br/>
+				 <br/>
 				 <CTA word='SCROLL THE FACTS'/>
 				 </div>
 				 </div>
--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,16 @@ const config = {
 				test: /\.css$/,
 				use: ['style-loader', 'css-loader'],
 			},
+			{
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            }
 		],
 	},
 	resolve: {
