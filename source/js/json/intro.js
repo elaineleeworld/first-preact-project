2b31b91e3e09b6f33eb524c4ae71ab3747287f7c
elaineleeworld <elaine.lee@elainel-mbp.ff0000.com>
2019-04-03 16:04:46 -0700

    add RED logo favicon

--- source/js/json/intro.js
+++ source/js/json/intro.js
@@ -2,9 +2,9 @@
 	slides: [
 	{
 		id: 0,
-		headline: 'Ads with impact.',
-		subheadline: "For most agencies, display and social ads are an afterthought. For us, they're, our story. We know how to make people stop scrolling - and start interacting."
-		cta: 'SCROLL THE FACTS'
+		// headline: 'Ads with impact.',
+		// subheadline: "For most agencies, display and social ads are an afterthought. For us, they're, our story. We know how to make people stop scrolling - and start interacting."
+		// cta: 'SCROLL THE FACTS'
 		url: '../../source/assets/images/intro_1_marvel.png'
 	},
 	{
