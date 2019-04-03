efd02eb9fd21ae75de066fbbdbd9fb30416c51ba
elaineleeworld <elaine.lee@elainel-mbp.ff0000.com>
2019-04-02 17:38:29 -0700

    carousel working

--- source/js/App.js
+++ source/js/App.js
@@ -8,7 +8,13 @@ class App extends Component {
 	render(props, states) {
 		return (
 			<div id='app'>
+			<div>
 				<Slideshow content={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/4723/slides.json`} cycleSpeed={3000} />
+			</div>
+			<br/>
+			<div>
+				<Slideshow content={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/4723/slides.json`} cycleSpeed={3000} />
+			</div>
 			</div>
 		)
 	}
