683a5be5d88d1cf756a77f96500cc158c0ec3476
Elaine Lee <elaine.lee@ff0000.com>
2019-05-20 17:21:32 -0700

    remove latest work links

--- source/js/App.js
+++ source/js/App.js
@@ -332,13 +332,13 @@ class App extends Component {
                         wait={1000}
                         headline="13x. That's how much faster we work."
                         subheadline="We've developed in-house ad tech that creates dynamic campaigns, at scale, and much faster than conventional production methods. Including a product called Velvet - our favorite toy."
-                        cta='DISCOVER VELVET'
-                        ctaStyles={{backgroundColor: '#232b3c'}}
+                        cta="DISCOVER VELVET"
+                        ctaStyles={{ backgroundColor: "#232b3c" }}
                         onClick={this.showModal}
                       />
 
                       <Slideshow content={data4} cycleSpeed={3000} />
-                    {/*  <CTA wait={1000} word="DISCOVER VELVET" onClick={this.showModal} /> */}
+                      {/*  <CTA wait={1000} word="DISCOVER VELVET" onClick={this.showModal} /> */}
                     </div>
                   </div>
 
@@ -357,11 +357,11 @@ class App extends Component {
                       wait={1000}
                       headline="6 partners consult with us about how to make their platforms work better."
                       subheadline="And we couldn't be prouder to work with them."
-                      imagesrc='assets/images/partner_logos.png'
+                      imagesrc="assets/images/partner_logos.png"
                     />
 
                     <Slideshow content={data6} cycleSpeed={3000} />
-                  { /* <div className="partner-logos">
+                    {/* <div className="partner-logos">
                       <img src="assets/images/partner_logos.png" />
                     </div> */}
                   </div>
@@ -382,13 +382,13 @@ class App extends Component {
                         wait={1000}
                         headline="Tons of happy clients use our skills every day."
                         subheadline="They're too many to count. But they include some big names in sports, streaming entertainment, and beverage - and one kinda famous mouse."
-                        cta='LATEST WORK'
-                        ctaStyles={{backgroundColor: '#232b3c'}}
-                        onClick={() => window.open("http://sizzle.wearered.com", "_blank")}
                       />
+                      {/* cta='LATEST WORK'*/}
+                      {/* ctaStyles={{backgroundColor: '#232b3c'}}*/}
+                      {/* onClick={() => window.open("http://sizzle.wearered.com", "_blank")} */}
 
                       <Slideshow content={data8} cycleSpeed={3000} />
-                     {/* <CTA wait={1000} word="LATEST WORK" onClick={() => window.open("http://sizzle.wearered.com", "_blank")} />*/}
+                      {/* <CTA wait={1000} word="LATEST WORK" onClick={() => window.open("http://sizzle.wearered.com", "_blank")} />*/}
                       {/*<CTA  wait={1000} word='LATEST WORK' onClick={this.showModal2} />
 				<Modal show={this.state.show2} url='https://www.youtube.com/watch?v=sNJUzwBNbxo' handleClose={this.hideModal2} /> */}
                     </div>
--- source/js/components/HamburgerOverlay.js
+++ source/js/components/HamburgerOverlay.js
@@ -1,47 +1,47 @@
-import { h, render, Component } from 'preact'
-import {Router} from 'preact-router';
-import { Link, Match } from 'preact-router/match';
-import Slideshow from'./Slideshow'
-import Modal from './Modal'
+import { h, render, Component } from "preact";
+import { Router } from "preact-router";
+import { Link, Match } from "preact-router/match";
+import Slideshow from "./Slideshow";
+import Modal from "./Modal";
 
-import ReactDelayRender from 'react-delay-render';
-import Delay from 'react-delay-render';
+import ReactDelayRender from "react-delay-render";
+import Delay from "react-delay-render";
 
-import '../../style/HamburgerOverlay.scss'
-
+import "../../style/HamburgerOverlay.scss";
 
 const HamburgerOverlay = ({ handleClose, handleClose2, handleClose3, handleClose4, show, children }) => {
-  const showHideClassName = show ? 'hamburger-overlay-container display-block' : 'hamburger-overlay-container display-none';
- 
+  const showHideClassName = show ? "hamburger-overlay-container display-block" : "hamburger-overlay-container display-none";
+
   return (
-  	<div className="hamburger-overlay-container" className={showHideClassName}>
-  	<img className='red-logo' src='assets/images/REDproduction.png'/>
-				<div className='hamburger-list-container'>
-					<div className='overlay-divider-line'>
-					{children}
+    <div className="hamburger-overlay-container" className={showHideClassName}>
+      <img className="red-logo" src="assets/images/REDproduction.png" />
+      <div className="hamburger-list-container">
+        <div className="overlay-divider-line">
+          {children}
 
-        		<ul>
-      <li href="/" onClick={handleClose} >Home</li>
-      <li href="/work" onClick={() => window.open('http://sizzle.wearered.com', "_blank")}>Latest Work</li>
-      <li href="/velvet" onClick={handleClose2}>Discover Velvet</li>
+          <ul>
+            <li href="/" onClick={handleClose}>
+              Home
+            </li>
+            {/* <li href="/work" onClick={() => window.open('http://sizzle.wearered.com', "_blank")}>Latest Work</li>*/}
+            <li href="/velvet" onClick={handleClose2}>
+              Discover Velvet
+            </li>
 
-      <li href="/contact" onClick={handleClose4}>Contact Us</li>
-    {/*  <li href="/careers" onClick={() => {
+            <li href="/contact" onClick={handleClose4}>
+              Contact Us
+            </li>
+            {/*  <li href="/careers" onClick={() => {
 							window.location.href = 'http://www.wearered.com/careers'
 						}}>Careers</li>*/}
-    </ul>
-    
-		 <img className='overlay-close-icon' src='assets/images/X.png'
-          onClick={handleClose}  />
-   
-					</div>
-				</div>
-			</div>
+          </ul>
 
-
-);
-}
-   
+          <img className="overlay-close-icon" src="assets/images/X.png" onClick={handleClose} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // export default ReactDelayRender({ delay: 1000 })(HamburgerOverlay);
 // export default Delay({ delay: 1000, onRender: render })(HamburgerOverlay)
